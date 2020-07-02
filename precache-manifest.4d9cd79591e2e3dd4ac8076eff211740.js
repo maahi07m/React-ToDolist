76eed68239e72ebc2614667df9669fdba23be934
maahi <gaddammahesh342@gmail.com>
2020-07-02 19:44:14 +0530

    Updates

---
+++ precache-manifest.4d9cd79591e2e3dd4ac8076eff211740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690926ccfc5ae2dda3ddb209ff0de140",
+    "url": "/mahireact/index.html"
+  },
+  {
+    "revision": "d3ddf13d7c02febaee80",
+    "url": "/mahireact/static/css/main.f382f80b.chunk.css"
+  },
+  {
+    "revision": "9fa0cb5b1338d8c0ff00",
+    "url": "/mahireact/static/js/2.026cec54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mahireact/static/js/2.026cec54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ddf13d7c02febaee80",
+    "url": "/mahireact/static/js/main.09ca9019.chunk.js"
+  },
+  {
+    "revision": "f2230b6f95e4490311a2",
+    "url": "/mahireact/static/js/runtime-main.4cf0110c.js"
+  },
+  {
+    "revision": "8b500a86007359f35381bdb13d317a34",
+    "url": "/mahireact/static/media/Liste.8b500a86.svg"
+  }
+]);
