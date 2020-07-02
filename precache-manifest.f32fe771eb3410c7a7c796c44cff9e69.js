be03169670d48d16993d10ea25355b925f088597
maahi <gaddammahesh342@gmail.com>
2020-07-02 19:30:33 +0530

    Updates

---
+++ precache-manifest.f32fe771eb3410c7a7c796c44cff9e69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ed9b857639fbb80fad318c303a8ef2",
+    "url": "/React-ToDolist/index.html"
+  },
+  {
+    "revision": "1f1bedf6c206addef575",
+    "url": "/React-ToDolist/static/css/main.f382f80b.chunk.css"
+  },
+  {
+    "revision": "9fa0cb5b1338d8c0ff00",
+    "url": "/React-ToDolist/static/js/2.026cec54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDolist/static/js/2.026cec54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1bedf6c206addef575",
+    "url": "/React-ToDolist/static/js/main.250f82b6.chunk.js"
+  },
+  {
+    "revision": "1cfb679b27036b6887d0",
+    "url": "/React-ToDolist/static/js/runtime-main.29a23f3d.js"
+  },
+  {
+    "revision": "8b500a86007359f35381bdb13d317a34",
+    "url": "/React-ToDolist/static/media/Liste.8b500a86.svg"
+  }
+]);
